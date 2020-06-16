f82b363f17144a834bd3811ace2ebd1f05d898d9
dxiaoping <1693547683@qq.com>
2020-06-16 11:48:30 +0800

    冲突修改

--- material-web/src/main.js
+++ material-web/src/main.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-<<<<<<< HEAD
-import './mock/index.js';  // 该项目所有请求使用mockjs模拟
-=======
+// import './mock/index.js';  // 该项目所有请求使用mockjs模拟s
 // import './mock/index.js';  // 该项目所有请求使用mockjs模拟
 import './login.js' 
 import './ui.js' 
