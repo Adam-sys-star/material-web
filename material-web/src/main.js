ad351ea8205390f8d90fb10fd38138f707eb8ec3
Adam <764708694@qq.com>
2020-06-16 13:45:58 +0800

    1111111111111

--- material-web/src/main.js
+++ material-web/src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './store';
-<<<<<<< HEAD
+
 
 //import './mock/index.js';  // 该项目所有请求使用mockjs模拟
 
-=======
-// import './mock/index.js';  // 该项目所有请求使用mockjs模拟s
->>>>>>> f82b363f17144a834bd3811ace2ebd1f05d898d9
-// import './mock/index.js';  // 该项目所有请求使用mockjs模拟
+
 import './login.js' 
 import './ui.js' 
 
