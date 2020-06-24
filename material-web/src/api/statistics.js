6911248ac6f873d08a7854b9ee532ea4358107b6
dxiaoping <1693547683@qq.com>
2020-06-24 16:22:12 +0800

    统计模块

--- material-web/src/api/statistics.js
+++ material-web/src/api/statistics.js
@@ -3,9 +3,9 @@ import fetch from 'utils/fetch';
 export function loadTurnoverData(params) {
 
 	console.log("params", params)
-	// return fetch({
-	// 	url: '/material/item_sale/sale_list',
-	// 	method: 'post',
-	// 	data
-	// });
+	return fetch({
+		url: '/material/statistics/turnover',
+		method: 'get',
+		data:params
+	});
 }
