16910aabe31b1dd88397ba18e8af8cd062cba8b9
dxiaoping <1693547683@qq.com>
2020-07-08 14:05:31 +0800

    message

--- material-web/src/router/index.js
+++ material-web/src/router/index.js
@@ -102,8 +102,9 @@ export const asyncRouterMap = [
 			  children:[
 				  // {path: 'Turnover',name: '会员账号管理',icon:'ios-analytics',component: _import('member/MemberManage')},
 				  {path: 'Turnover',name: '营业额统计',icon:'ios-analytics',component: _import('statistics/Turnover')},
-				  {path: 'Bestseller',name: '畅销榜',icon:'ios-analytics',component: _import('statistics/Bestseller')},
-				  // {path: 'test',name: '测试',icon:'ios-analytics',component: _import('statistics/ItemClass')}]
+				  {path: 'Bestseller',name: '畅销榜',icon:'ios-analytics',component: _import('statistics/Bestseller')}
+				  // {path: 'test',name: '测试',icon:'ios-analytics',component: _import('statistics/ItemClass')},
+					]
 
 		},
       {path: '/sale_list',name: '销售记录',icon:"android-list",meta: { role:["超级管理员"] },component: _import('goodsSale/SaleList')},
